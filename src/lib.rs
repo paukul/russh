26dd31ea5b7977e1ac453cdcb1f3baa28c8e4a0b
Pascal Friederich <paukul@gmail.com>
2018-06-05 23:56:10 +0200

    this time for real

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,8 @@ use byteorder::{ByteOrder, BigEndian};
 
 mod packet;
 
+use packet::Packet;
+
 // SSH_MSG_DISCONNECT             1
 // SSH_MSG_IGNORE                 2
 // SSH_MSG_UNIMPLEMENTED          3
@@ -24,7 +26,7 @@ mod packet;
 
 #[allow(non_camel_case_types)]
 #[derive(Debug)]
-enum MSG_TYPE {
+pub enum MSG_TYPE {
     SSH_MSG_DISCONNECT,
     SSH_MSG_IGNORE,
     SSH_MSG_UNIMPLEMENTED,
@@ -33,32 +35,6 @@ enum MSG_TYPE {
     SSH_MSG_SERVICE_ACCEPT,
     SSH_MSG_KEXINIT,
     SSH_MSG_NEWKEYS
-}
-
-#[allow(dead_code)]
-struct Packet {
-    packet_length: u32,
-    payload: Vec<u8>,
-    padding: Vec<u8>,
-    mac: Vec<u8>
-}
-
-impl Packet {
-    fn new(buf: &[u8]) -> Packet {
-        let packet_length = BigEndian::read_u32(&buf[0..4]);
-        let padding_length = u32::from(buf[4]);
-        let payload_length = packet_length - padding_length - 1;
-        let last_payload_byte = payload_length - padding_length + 4;
-        let payload = &buf[5..last_payload_byte as usize];
-        trace!("Packet Length: {}", packet_length);
-        trace!("Padding Length: {}", padding_length);
-        Packet {
-            packet_length,
-            payload: payload.to_vec(),
-            padding: Vec::new(),
-            mac: Vec::new()
-        }
-    }
 }
 
 impl From<u8> for MSG_TYPE {
@@ -85,44 +61,37 @@ pub fn connect(host: &str, port: u16) -> Result<(), Error> {
     let socket_address = format!("{}:{}", host, port);
     debug!("connecting to {}", socket_address);
     let mut stream = TcpStream::connect(socket_address)?;
-    let mut buf = String::new();
-
-    let mut raw_reader = stream.try_clone()?;
     let mut reader = BufReader::new(stream.try_clone()?);
+    let mut buf = String::new();
     reader.read_line(&mut buf)?;
     debug!("Server version: {}", buf.trim_right());
-
     stream.write_all(format!("{}\r\n", VERSION).as_bytes())?;
     stream.flush()?;
 
-    let mut buf = [0; MAX_PACKET_SIZE];
-    let read = raw_reader.read(&mut buf)?;
-    let buf = &buf[0..=read];
-    let packet = Packet::new(&buf);
-    let msg_type = packet.payload[0];
-    debug!("Msg type: {:?}", MSG_TYPE::from(msg_type));
+    let mut packet = Packet::read_from(&mut reader)?;
+    debug!("Msg type: {:?}", packet.msg_type());
 
-    let (_, mut buf) = packet.payload.split_at(17);
-    let mut algorithms = consume_string(&mut buf);
-    debug!("Kex algorithms: {}", String::from_utf8(algorithms.to_vec())?);
-    algorithms = consume_string(&mut buf);
-    debug!("server_host_key_algorithms: {}", String::from_utf8(algorithms.to_vec())?);
-    algorithms = consume_string(&mut buf);
-    debug!("encryption_algorithms_client_to_server: {}", String::from_utf8(algorithms.to_vec())?);
-    algorithms = consume_string(&mut buf);
-    debug!("encryption_algorithms_server_to_client: {}", String::from_utf8(algorithms.to_vec())?);
-    algorithms = consume_string(&mut buf);
-    debug!("mac_algorithms_client_to_server: {}", String::from_utf8(algorithms.to_vec())?);
-    algorithms = consume_string(&mut buf);
-    debug!("mac_algorithms_server_to_client: {}", String::from_utf8(algorithms.to_vec())?);
-    algorithms = consume_string(&mut buf);
-    debug!("compression_algorithms_client_to_server: {}", String::from_utf8(algorithms.to_vec())?);
-    algorithms = consume_string(&mut buf);
-    debug!("compression_algorithms_server_to_client: {}", String::from_utf8(algorithms.to_vec())?);
-    algorithms = consume_string(&mut buf);
-    debug!("languages_client_to_server: {}", String::from_utf8(algorithms.to_vec())?);
-    algorithms = consume_string(&mut buf);
-    debug!("languages_server_to_client: {}", String::from_utf8(algorithms.to_vec())?);
+    // let (_, mut buf) = packet.payload.split_at(17);
+    // let mut algorithms = consume_string(&mut buf);
+    // debug!("Kex algorithms: {}", String::from_utf8(algorithms.to_vec())?);
+    // algorithms = consume_string(&mut buf);
+    // debug!("server_host_key_algorithms: {}", String::from_utf8(algorithms.to_vec())?);
+    // algorithms = consume_string(&mut buf);
+    // debug!("encryption_algorithms_client_to_server: {}", String::from_utf8(algorithms.to_vec())?);
+    // algorithms = consume_string(&mut buf);
+    // debug!("encryption_algorithms_server_to_client: {}", String::from_utf8(algorithms.to_vec())?);
+    // algorithms = consume_string(&mut buf);
+    // debug!("mac_algorithms_client_to_server: {}", String::from_utf8(algorithms.to_vec())?);
+    // algorithms = consume_string(&mut buf);
+    // debug!("mac_algorithms_server_to_client: {}", String::from_utf8(algorithms.to_vec())?);
+    // algorithms = consume_string(&mut buf);
+    // debug!("compression_algorithms_client_to_server: {}", String::from_utf8(algorithms.to_vec())?);
+    // algorithms = consume_string(&mut buf);
+    // debug!("compression_algorithms_server_to_client: {}", String::from_utf8(algorithms.to_vec())?);
+    // algorithms = consume_string(&mut buf);
+    // debug!("languages_client_to_server: {}", String::from_utf8(algorithms.to_vec())?);
+    // algorithms = consume_string(&mut buf);
+    // debug!("languages_server_to_client: {}", String::from_utf8(algorithms.to_vec())?);
 
     // let (_, tail) = packet.payload.split_at(17);
     // let str_length = BigEndian::read_u32(&tail[0..4]) as usize;
--- src/packet.rs
+++ src/packet.rs
@@ -1,16 +1,52 @@
 use std::io::{self, Read};
-use byteorder::{BigEndian, ReadBytesExt};
+use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
 use failure::Error;
+use super::MSG_TYPE;
 
 pub struct Packet {
-  payload: Vec<u8>
+  raw: Vec<u8>,
+  padding_length: u8,
+  payload_length: usize,
+  pos: usize
 }
 
 impl Packet {
-  fn read_from<R: io::Read>(stream: &mut R) -> Result<Packet, Error> {
-    let payload_length = stream.read_u32::<BigEndian>()? as usize;
-    let mut payload = Vec::with_capacity(payload_length);
-    let read = stream.take(payload_length as u64).read_to_end(&mut payload);
-    Ok(Packet{payload})
+  pub fn read_from<R: io::Read>(stream: &mut R) -> Result<Packet, Error> {
+    let packet_length = stream.read_u32::<BigEndian>()? as usize;
+    let mut raw = Vec::with_capacity(packet_length);
+    &raw.write_u32::<BigEndian>(packet_length as u32);
+    let read = stream.take(packet_length as u64).read_to_end(&mut raw)?;
+    let padding_length = raw[4];
+    let payload_length = packet_length - usize::from(padding_length) - 1;
+    if read < packet_length {
+      Err(io::Error::new(io::ErrorKind::BrokenPipe, "broken stream").into())
+    } else {
+      Ok(Packet{
+          raw,
+          padding_length,
+          payload_length,
+          pos: 5
+        }
+      )
+    }
+  }
+
+  pub fn msg_type(self) -> MSG_TYPE {
+    trace!("Msg Type: {}", self.raw[5]);
+    MSG_TYPE::from(self.raw[5])
+  }
+}
+
+impl Read for Packet {
+  fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+    let read_to = self.pos + buf.len();
+    if read_to > self.payload_length {
+      Err(io::Error::new(io::ErrorKind::UnexpectedEof, "packet end reached").into())
+    } else {
+      let mut reader = &self.raw[self.pos..read_to];
+      let read = reader.read(buf)?;
+      self.pos = self.pos + read - 1;
+      Ok(read)
+    }
   }
 }
